2eb7a8f0d8434051499b187f0c8c88942f740fb5
Miguel De Pelsmaeker <migueleken@hotmail.com>
2020-09-27 22:23:44 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env, {mode}) => {
+  console.log(mode);
+  return {
+    output: {
+      filename: '[name].[hash].js'
+    },
+    devServer: {
+      overlay: true,
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-srcsets-loader',
+              options: {
+                attrs: [':src', ':srcset']
+              }
+            }
+          ]
+        },  
+        {
+          test: /\.(jpe?g|png|svg|webp|ico)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 1000,
+              context: './src',
+              name: '[path][name].[ext]'
+            }
+          }
+        },
+        {
+          test: /\.(woff|woff2|ttf|eot)$/,
+          use: 'file-loader?name=fonts/[name].[ext]!static'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            mode === 'production'
+              ? MiniCssExtractPlugin.loader
+              : 'style-loader',
+            'css-loader',
+            'resolve-url-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  require('postcss-import'),
+                  postcssPresetEnv({stage: 0})
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+        title: 'ThreeJS webGL thingy'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.[contenthash].css'
+      }),
+      new OptimizeCSSAssetsPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  };
+};
